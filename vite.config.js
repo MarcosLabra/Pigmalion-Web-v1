0af11d2af5ab004d1f25526d3a898f9c40af27aa
Marcos <mlabra.dev@gmail.com>
2026-08-08 14:13:22 -0300

    cambio ruta base de vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import { ghPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  base: '/Pigmalion-Web-V1/',
+  base: '/Pigmalion-Web-v1/',
   plugins: [ghPages()],
 })
